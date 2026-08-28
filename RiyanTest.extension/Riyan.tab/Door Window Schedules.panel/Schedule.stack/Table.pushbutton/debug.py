d6b2c61ff7414d61b995b94ac025f244abf087ec
Dilupa <dilupa1990@gmail.com>
2026-08-28 10:52:50 +0530

    Fix elevation height scaling for tall doors by setting the Elevation Height parameter explicitly

--- RiyanTest.extension/Riyan.tab/Door Window Schedules.panel/Schedule.stack/Table.pushbutton/debug.py
+++
@@ -1,6 +0,0 @@
-import clr
-clr.AddReference('RevitAPI')
-import Autodesk.Revit.DB as DB
-def execute(doc):
-    with open('C:\\Users\\User\\Desktop\\TEST\\sheet_test.txt', 'w') as f:
-        f.write(str(hasattr(DB.Viewport, 'GetLabelOutline')))
--- RiyanTest.extension/Riyan.tab/Door Window Schedules.panel/Schedule.stack/Table.pushbutton/debug2.py
+++
@@ -1,9 +0,0 @@
-import clr
-clr.AddReference('RevitAPI')
-import Autodesk.Revit.DB as DB
-
-import sys
-
-props = [p for p in dir(DB.ViewSheet) if 'collection' in p.lower()]
-with open('C:\\Users\\User\\Desktop\\TEST\\sheet_collection_test.txt', 'w') as f:
-    f.write(str(props))
